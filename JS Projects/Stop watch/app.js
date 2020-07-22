7c9ef31c2b986de3dfda6a873b07a8e9a9400913
SARFRAZ RASHEED <56587381+Sarfraz123-wq@users.noreply.github.com>
2020-07-22 20:23:28 +0500

    Stop watch

---
+++ JS Projects/Stop watch/app.js
@@ -0,0 +1,46 @@
+let start = document.querySelector('.start');
+let stop = document.querySelector('.stop');
+let reset = document.querySelector('.reset');
+let mints = document.querySelector('.mints');
+let sec = document.querySelector('.sec');
+let msec = document.querySelector('.msec');
+// start.addEventListener('click',startWatch);
+// stop.addEventListener('click',stopWatch);
+// reset.addEventListener('click',resetWatch);
+
+
+let count=0;
+let timesec=0;
+let timemints=0;
+function startWatch () {
+     abc = setInterval(() => {
+       if(count==100){
+           timesec++;
+           sec.innerHTML = timesec;
+           count=0;
+       }
+       if(timesec==5){
+        timemints++;
+        mints.innerHTML = timemints;
+        timesec=0;
+    }
+   count++;
+   msec.innerHTML = count;
+}, 10);  
+    start.disabled = true;
+};
+function stopWatch() {
+    clearInterval(abc);
+    start.disabled = false;
+}
+function resetWatch(){
+    count=0;
+    timesec=0;
+    timemints=0;
+    msec.innerHTML = 0;
+    sec.innerHTML = 0;
+    mints.innerHTML = 0;
+    stopWatch();
+    start.disabled = false;
+}
+    
